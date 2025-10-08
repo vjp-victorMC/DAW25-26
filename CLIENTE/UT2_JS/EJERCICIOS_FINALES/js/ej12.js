a13f2724d3af14c9885a4f80bd32b561814e9e32
vjp-victorMC <vmariscalc01@informatica.iesvalledeljerteplasencia.es>
2025-10-08 13:39:46 +0200

    aa

--- CLIENTE/UT2_JS/EJERCICIOS_FINALES/js/ej12.js
+++ CLIENTE/UT2_JS/EJERCICIOS_FINALES/js/ej12.js
@@ -13,12 +13,15 @@ let estudiante3 = {
 }
 let estudiante4 = {
     nombre: "Julia",
-    notas: [1, 8, 5, 3, 10]
+    notas: [1, 8, 5, 13, 10]
 }
 
 let mapaEstudiantes = new Map();
 
 mapaEstudiantes.set(estudiante1.nombre, estudiante1.notas);
+mapaEstudiantes.set(estudiante2.nombre, estudiante2.notas);
+mapaEstudiantes.set(estudiante3.nombre, estudiante3.notas);
+mapaEstudiantes.set(estudiante4.nombre, estudiante4.notas);
 
 function mediaAritmetica(...notas) {
     let sumaTotal = notas.reduce((total, nota) => total + nota, 0);
@@ -29,16 +32,65 @@ function mediaAritmetica(...notas) {
 
 }
 
-function pintarMedia(estudiante) {
-    console.log("Nombre: " + estudiante.nombre);
-    console.log("Media: " + mediaAritmetica(...estudiante.notas))
-    console.log("\n");
+function mostrarEstudiantes(mapaEstudiantes) {
+    mapaEstudiantes.forEach((notas, nombre) => {
+        console.log("Nombre: " + nombre);
+        console.log("Media: " + mediaAritmetica(...notas))
+        console.log("\n");
+    });
 }
 
-pintarMedia(estudiante1);
-pintarMedia(estudiante2);
-pintarMedia(estudiante3);
-pintarMedia(estudiante4);
+mostrarEstudiantes(mapaEstudiantes);
+
+
+function pintarMejorAlumno(mapaEstudiantes) {
+    let mejorMedia = 0;
+    let mejorAlumno = "";
+
+    mapaEstudiantes.forEach((notas, nombre) => {
+        let notaMedia = mediaAritmetica(...notas);
+        if (notaMedia > mejorMedia) {
+            mejorMedia = notaMedia;
+            mejorAlumno = nombre;
+        }
+    });
+
+    console.log("El mejor alumno es " + mejorAlumno + " con una nota media de " + mejorMedia);
+}
+
+pintarMejorAlumno(mapaEstudiantes);
+
+
+function ordenarEstudiantes(mapaEstudiantes) {
+    let arrayEstudiantes = Array.from(mapaEstudiantes);
+
+    for (let i = 0; i < arrayEstudiantes.length; i++) {
+        for (let j = 0; j < arrayEstudiantes[i].length; j++) {
+            if (j = 1) {
+                arrayEstudiantes[i][j] = mediaAritmetica(...arrayEstudiantes[i][j]);
+            }
+        }
+    }
+
+    arrayEstudiantes.sort((a, b) => b[1] - a[1]);
+
+    let estudiantesOrdenados = new Map(arrayEstudiantes);
+}
+
+ordenarEstudiantes(mapaEstudiantes);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
