fcdc2a8b431d12e6b4cb039562e32d34eca42a61
vjp-victorMC <vmariscalc01@informatica.iesvalledeljerteplasencia.es>
2025-11-07 10:50:51 +0100

    aa

---
+++ REPASO-CLIENTE/UT3/js/prueba.js
@@ -0,0 +1,22 @@
+
+// console.log("1. " + new Date().toString());
+// let timeoutID = setTimeout(() => console.log("2. " + new Date().toString()), 5000);
+// console.log("3. " + new Date().toString());
+
+// if (prompt("Desea parar la función?(yes/no)") === "yes") {
+//     clearTimeout(timeoutID);
+// }
+
+
+// let num = 1;
+// let idInterval = setInterval(function () {
+//     console.log(++num);
+//     if (num > 10) {
+//         clearInterval(idInterval);
+//     }
+// }, 1000);
+
+
+
+
+
